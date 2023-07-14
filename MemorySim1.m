d3aa87c1147031987f95f1b766c26a4302a52089
Harsh Jain <harshjainldh@gmail.com>
2023-07-14 08:11:09 +0530

    Update MemorySim1.m

--- MemorySim1.m
+++ MemorySim1.m
@@ -1,12 +1,17 @@
 clear all
 close all
+
+
+global Fswitch;
+Fswitch = 0; % This switch decides when to dis-engage indentor force
+
 tic
 % Parameters
 m  = 1;     % mass
 b  = 10;    % damping constant
-k  = 10;  % spring consta
-F0 = 1;     % amplitude of external force
-F_fric_mag = F0*1;
+k  = 20;  % spring consta
+F0 = 2;     % amplitude of external force
+F_fric_mag = 2;
 f=F_fric_mag;
 x_eq = f/k;
 
@@ -28,28 +33,40 @@ xlabel('Time (s)');
 ylabel('Displacement (m)');
 % t
 %% 
-% ForceMat = zeros(size(t));
-% for i=1:length(t)    
-%     ForceMat(i)=STEPfunc(1,t(i),y(i,:),f);
-% end
-% %%
-% figure
-% plot(t,ForceMat)
-% title('Force')
+tMat = (0:0.1:100)';
+F_appliedMat=[ceil(100*tMat(1:50))/100 * F0 ; ...
+    10*F0-F0*ceil(tMat(51:100)*100)/100; 0*tMat(101:end)];
+
+F_applied = interp1(tMat,F_appliedMat,t);
+
+ForceMat = zeros(size(t));
+for i=1:length(t)    
+    ForceMat(i)=STEPfunc(F0,t(i),k,b,y(i,:),f);
+end
+%
 figure
+yyaxis left
+plot(t,ForceMat)
+yyaxis right
 plot(t,y(:,2))
 title('velocity')
-    %%
+
+figure
+plot(t,F_applied)
+title('Force')
+
 figure
 hold on
 plot(t, y(:, 1));
 % plot(t,ForceMat)
 %%
 % figure;
-% plot(y(1:10:end,1),ForceMat(1:10:end))
+% % plot(y(1:10:end,1),ForceMat(1:10:end))
 % ylim([0 10])
+% 
+% % Force is F_ind=F_fric-ky
+% % Indentation is not the same as y. 
+% toc
+% disp(max(y(:,1)))
 
-% Force is F_ind=F_fric-ky
-% Indentation is not the same as y. 
-toc
-disp(max(y(:,1)))
+%%
